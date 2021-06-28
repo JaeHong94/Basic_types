caec526b206ea05b5e8a08a47cd917867056d183
JaeHongMin94 <moonshin123@naver.com>
2021-06-28 21:54:40 +0900

    Array type

---
+++ array.ts
@@ -0,0 +1,7 @@
+let list: number[] = [1, 2, 3];
+
+// 밑에 방식은 tsx 또는 jsx에서 충돌날 가능성이 있어서 위에 방식을 선호
+// let list: Array<number> = [1, 2, 3];
+
+// 여러가지 타입을 배열에 넣고싶을 경우
+let info: (number | string)[] = [28, 'JaeHong'];
