17d28013764a6c4fc559ba1a0691e0ec2e81d9c0
JaeHongMin94 <moonshin123@naver.com>
2021-06-28 14:51:42 +0900

    string type

---
+++ string.js
@@ -0,0 +1,5 @@
+"use strict";
+let myName = 'JaeHong';
+let age = 28;
+let sentence = `Hello, My name is ${myName}. I'm ${age} years old.`;
+console.log(sentence);
---
+++ string.ts
@@ -0,0 +1,6 @@
+let myName: string = 'JaeHong';
+let age: number = 28;
+
+let sentence: string = `Hello, My name is ${myName}. I'm ${age} years old.`;
+
+console.log(sentence);
