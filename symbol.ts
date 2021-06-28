b8fd3f8ced3a1dc79a3b74b2e121dca87deb9a0e
JaeHongMin94 <moonshin123@naver.com>
2021-06-28 19:59:59 +0900

    Symbol type

---
+++ symbol.ts
@@ -0,0 +1,13 @@
+console.log(Symbol('foo') === Symbol('foo'));
+
+const sym = Symbol();
+const obj = {
+  [sym]: 'value',
+};
+
+console.log(obj[sym]);
+/*
+  원시타입의 값을 담아서 사용.
+  고유하고 수정불가능한 값으로 만든다.
+  보통 접근을 제어할때 사용하는 경우가 많음
+*/
